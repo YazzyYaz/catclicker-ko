8833c04f225f371f3b0732062668f6256d290684
Yaz Khoury <yaz.khoury@gmail.com>
2015-09-23 22:46:40 -0400

    feat: Add titles using with and for control flow

--- js/app.js
+++ js/app.js
@@ -1,20 +1,48 @@
-var ViewModel = function() {
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/cat.jpg');
 	this.imgAttribution = ko.observable('');
 
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	}
-
-	self.nickname = ko.observableArray([
+	this.nickname = ko.observableArray([
 		{nick: 'Tab'},
 		{nick: 'Tobes'},
 		{nick: 'Tabu'},
 		{nick: 'Tootsie'},
 		{nick: 'Toker'}
 	]);
+
+	this.title = ko.computed(function() {
+		var title;
+		var clicks = this.clickCount();
+		if (clicks < 10) {
+			title = 'Baby';
+		} else if (clicks < 20) {
+			title = 'Infant';
+		} else if (clicks < 30) {
+			title = 'Kid';
+		} else if (clicks < 40) {
+			title = 'Teen';
+		} else if (clicks < 60) {
+			title = 'Hipster';
+		} else if (clicks < 80) {
+			title = 'Big Guy';
+		} else {
+			title = 'Old Man';
+		}
+		return title;
+	}, this);
 }
+
+var ViewModel = function() {
+	
+	this.currentCat = ko.observable( new Cat() );
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	}
+}
+
+
 
 ko.applyBindings(new ViewModel());
